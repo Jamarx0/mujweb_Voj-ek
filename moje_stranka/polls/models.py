4fc653837ba4c0a2363c3a16d19b78a3789bb7b8
Jakub Vojáček <jakubvojacek04@gmail.com>
2024-01-16 09:30:48 +0100

    spusteni sql.lite

--- moje_stranka/polls/models.py
+++ moje_stranka/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 class fff(models.Model):
-    nahodnepolicko=models.CharField
+    autor = models.CharField(max_length=200, default="x")
+    nazev = models.CharField(max_length=200, default="x")
+    prispevek = models.CharField(max_length=200, default="x")
 # Create your models here.
